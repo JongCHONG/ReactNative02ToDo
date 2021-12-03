ccf5cbeb535c7356f99033bf2111b8f82c9e5e13
JongCHONG <yonhoa27@hotmail.fr>
2021-12-03 13:35:04 +0100

    formik

--- components/ListModal.js
+++ components/ListModal.js
@@ -1,19 +1,33 @@
 import React from "react"
-import { useForm } from "react-hook-form"
 import { 
   Text,
-  StyleSheet
+  StyleSheet,
+  View,
+  Button,
 } from "react-native"
 import { TextInput } from "react-native-gesture-handler"
+
+import { Formik } from 'formik'
 
 const ListModal = props => {
 
   console.log(props)
   return (
-    <TextInput 
-      style={styles.input}
-      placeholder="Titre"
-    />
+    <Formik
+      initialValues={{ email: '' }}
+      onSubmit={values => console.log(values)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <TextInput
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            value={values.email}
+          />
+          <Button onPress={handleSubmit} title="Submit" />
+        </View>
+      )}
+    </Formik>
   )
 }
 
