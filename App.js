954a597ee4da01afdae23b243a93afe2a28d1b9f
JongCHONG <yonhoa27@hotmail.fr>
2021-12-03 16:39:29 +0100

    add a list

--- App.js
+++ App.js
@@ -8,15 +8,31 @@ import {
   Modal,
   TouchableOpacity,
 } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+
 import ListModal from './components/ListModal'
 
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false)
-  const [list, setList] = useState(null)
+  const [list, setList] = useState([])
 
   const handleSetList = (e) => {
-    console.log(e)
-    // setList([...list], setList(e))
+    // console.log(e)
+    setList([...list, e])
+  }
+
+  const ShowList = ({ item }) => {
+    // console.log(item)
+    return (
+      <View style={styles.item}>
+        <View style={styles.itemTitle}>
+          <Text>{item.title}</Text>
+        </View>
+        <View style={styles.itemDescription}>
+          <Text>{item.description}</Text>
+        </View>
+      </View>
+    )
   }
 
   console.log(list)
@@ -39,6 +55,19 @@ export default function App() {
         title="Add a list" 
         onPress={() => setIsModalVisible(true)}
       />
+      <View>
+        <View style={styles.list}>
+          <View style={styles.listtitle}>
+            <Text>Title</Text>
+          </View>
+          <View style={styles.listDescription}>
+            <Text>Description</Text>
+          </View>
+        </View>
+        {list &&
+          <FlatList data={list} renderItem={ShowList} />
+        }
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -50,5 +79,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  list: {
+    margin: 10,
+    flexDirection: 'row',
+  },
+  listtitle: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+    width: 200,
+    marginHorizontal: 5,
+  },
+  listDescription: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+    width: 200,
+    marginHorizontal: 5,
+  },
+  item: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+  },
+  itemTitle: {
+    width: 200,
+    marginHorizontal: 5,
+  },
+  itemDescription: {
+    width: 200,
+    marginHorizontal: 5,
   },
 });
--- components/ListModal.js
+++ components/ListModal.js
@@ -10,19 +10,25 @@ import { TextInput } from "react-native-gesture-handler"
 import { Formik } from 'formik'
 
 const ListModal = props => {
-
   console.log(props)
   return (
     <Formik
-      initialValues={{ email: '' }}
-      onSubmit={values => console.log(values)}
+      initialValues={{ title: '', description: '' }}
+      onSubmit={values => props.setList(values)}
     >
       {({ handleChange, handleBlur, handleSubmit, values }) => (
         <View>
           <TextInput
-            onChangeText={handleChange('email')}
-            onBlur={handleBlur('email')}
-            value={values.email}
+            onChangeText={handleChange('title')}
+            onBlur={handleBlur('title')}
+            value={values.title}
+            placeholder="Title"
+          />
+          <TextInput
+            onChangeText={handleChange('description')}
+            onBlur={handleBlur('description')}
+            value={values.description}
+            placeholder="Description"
           />
           <Button onPress={handleSubmit} title="Submit" />
         </View>
